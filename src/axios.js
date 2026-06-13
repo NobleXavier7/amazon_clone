28c7e37112e21537207476bea9d3f090222c6f4c
Noble Xavier <noblexavier2007@gmail.com>
2026-06-13 10:37:33 +0530

    chore: update axios baseURL to point to live render server

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://us-central1-clone-31b80.cloudfunctions.net/paymentApi"
+  baseURL: "https://amazon-clone-backend-l2ps.onrender.com"
   //'https://api-nzbfncg45q-uc.a.run.app'
   //'http://127.0.0.1:5001/clone-31b80/us-central1/api'
 
